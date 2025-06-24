c386509b0efe4c82f33a9b589192e66f5e22f8d8
azeezabidoye <azeezabidoye@yahoo.com>
2025-06-24 17:19:55 +0100

    Deploy script updated

--- deploy/00-deploy-funding-xfi.cjs
+++ deploy/00-deploy-funding-xfi.cjs
@@ -1,11 +1,11 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  await deploy("FundingXFI", {
-    contract: "FundingXFI",
-    from: deployer,
-    args: [], // Constructor paramenters
-    log: true,
-  });
-};
-module.exports.tags = ["FundingXFI"];
+// module.exports = async ({ getNamedAccounts, deployments }) => {
+//   const { deploy } = deployments;
+//   const { deployer } = await getNamedAccounts();
+//   await deploy("FundingXFI", {
+//     contract: "FundingXFI",
+//     from: deployer,
+//     args: [], // Constructor paramenters
+//     log: true,
+//   });
+// };
+// module.exports.tags = ["FundingXFI"];
