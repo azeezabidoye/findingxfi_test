3e4f80bc8d4b7e49d5a7bcd80e91cc64cc766ddc
azeezabidoye <azeezabidoye@yahoo.com>
2025-06-24 17:17:47 +0100

    First Commit

---
+++ contracts/FundingXFI.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract FundingXFI {
+    address public owner;
+    uint256 public minimumAmount = 20 * 1e18; // ETH to Wei
+    mapping(address => uint256) public addressToAmountFunded;
+    mapping(address => bool) private hasFunded;
+    address[] private funders;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() external payable {
+        require(msg.value > minimumAmount, "Send enough funds!");
+
+        if (!hasFunded[msg.sender]) {
+            hasFunded[msg.sender] = true;
+            funders.push(msg.sender);
+        }
+
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() external onlyOwner {
+        for (uint256 i = 0; i < funders.length; ) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+            hasFunded[funder] = false;
+            unchecked {
+                ++i;
+            } // Save gas by skipping overflow check
+        }
+
+        delete funders;
+
+        (bool success, ) = payable(owner).call{value: address(this).balance}(
+            ""
+        );
+        require(success, "Withdraw failed!");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner!");
+        _;
+    }
+
+    function getFunders() external view returns (address[] memory) {
+        return funders;
+    }
+}
---
+++ hardhat.config.cjs
@@ -0,0 +1,32 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const { PRIVATE_KEY, CROSSFI_ALCHEMY_URL } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    crossfiTestnet: {
+      url: CROSSFI_ALCHEMY_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 4157,
+    },
+    namedAccounts: {
+      deployer: {
+        default: 0,
+      },
+    },
+    gasReporter: {
+      enabled: true,
+      currency: "USD",
+      coinmarketcap: process.env.COINMARKETCAP_API_KEY, // Optional if you want real cost estimate
+      showTimeSpent: true,
+      excludeContracts: [],
+      noColors: false,
+    },
+  },
+};
