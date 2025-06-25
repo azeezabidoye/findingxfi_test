6b00f1cc99c3c04d632310ebc23fec4a99c85e89
azeezabidoye <azeezabidoye@yahoo.com>
2025-06-25 11:05:00 +0100

    Config updated

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -12,8 +12,8 @@ module.exports = {
   networks: {
     hardhat: { chainId: 1337 },
     crossfiTestnet: {
-      url: "rpc.testnet.ms",
-      accounts: [`0x${PRIVATE_KEY}]`],
+      url: "https://crossfi-testnet.g.alchemy.com/v2/QvGoIVVwyzXDCki9k_BIt",
+      accounts: [PRIVATE_KEY],
       chainId: 4157,
     },
     // namedAccounts: {
